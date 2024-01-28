c11bf741de0342c7698eb1d554171e1d936d5684
Himanshu Choudhary <raptordev21@gmail.com>
2024-01-28 16:03:50 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from '@/components/ui/button'
+
+function App() {
+
+  return (
+    <div className='p-16'>
+      <Button>Hello World</Button>
+    </div>
+  )
+}
+
+export default App
